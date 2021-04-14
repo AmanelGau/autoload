f16fb932841b1a45cdc470d5da5bfa5e279341b7
Amanel <amanel.gauriat@gmail.com>
2021-04-14 11:34:30 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'wilder/composer',
+  ),
+  'versions' => 
+  array (
+    'wilder/composer' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
